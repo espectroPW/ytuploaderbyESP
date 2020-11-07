1b0b657d25171c7c5cd9fc852c7d76f5a95fccc3
espectroPW <72676549+espectroPW@users.noreply.github.com>
2020-11-07 01:53:58 +0100

    Update skryptUpload.py

--- skryptUpload.py
+++ skryptUpload.py
@@ -12,17 +12,27 @@ nazwaPliku = ""
 tytul = ""
 kategoria = ""
 
-matrix = open('/home/pi/Skrypty/ytupload/wideo/nazwy.csv').read()
-print (matrix)
-matrix = [item.split() for item in matrix.split('\n')[:-1]]
-print("############################################")
+# matrix = open('/home/pi/Skrypty/ytupload/wideo/nazwy.csv').read()
+# print (matrix)
+# matrix = [item.split() for item in matrix.split('\n')[:-1]]
+# print("############################################")
 
 
-print (matrix)
-print (matrix[2])
+a_file = open("/home/pi/Skrypty/ytupload/wideo/nazwy.csv")
+file_contents = a_file.read()
+contents_split = file_contents.splitlines()
+
+print(contents_split)
+a_file.close()
+
+
+
+
+matrix = contents_split
+
+
 
 print ("Matrix[2}=",matrix[2])
-
 
 x = len(matrix)
 y = len(matrix[:0])
@@ -30,15 +40,15 @@ print ("X:{}".format(x))
 print ("X:{}".format(y))
 
 
-print("############################################")
 
 
 for xx in range(0,x):
+
     #print("Matrix:{}".format(matrix[xx]))
-    print("NazwaPliku:{}".format(matrix[xx]))
- 
     
+
+    print("NazwaPliku:{}".format(matrix[xx]))
     upload = ("python upload_video.py --file=/home/pi/Skrypty/ytupload/wideo/{} --title={} --category={} --privacyStatus={}".format({},"Test{}","{}","{}").format(matrix[xx],xx,22,"private"))
     print(upload)
-    #os.system(upload)
+    os.system(upload)
     
